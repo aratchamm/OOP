8fc9fc05122d325e7f4df48e589b677ba311ee97
aratcha <aratchamm@gmail.com>
2022-03-29 23:03:06 +0700

    add

---
+++ java lab/Firstjavafx/src/MainSceneController.java
@@ -0,0 +1,15 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class MainSceneController {
+
+    @FXML
+    private TextField trTitle;
+
+    @FXML
+    void btnClicked(ActionEvent event) {
+        
+    }
+
+}
